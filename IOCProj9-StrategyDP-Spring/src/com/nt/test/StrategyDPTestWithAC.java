f8d6a829a71b42875b9c4c705aa2dd6aeb122847
Roshan Acharya <Dev1@gmail.com>
2024-06-29 10:46:57 +0545

    app9

---
+++ IOCProj9-StrategyDP-Spring/src/com/nt/test/StrategyDPTestWithAC.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StrategyDPTestWithAC {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+
+
+	}
+
+}
